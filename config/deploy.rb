ad343fbae9111cbf26de9d8292513b1e1583959c
Michael Scharkow <michael@underused.org>
2012-07-09 08:20:31 +0200

    Better deploy handling

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,29 +7,37 @@ set :user, "deploy"
 set :domain, "#{user}@newsclassifier.org"
 set :deploy_to, "/home/deploy/apps/#{application}"
 
+set :rvm_cmd, "source ~/.rvm/scripts/rvm && rvm rvmrc trust #{deploy_to}/current && cd #{deploy_to}/current"
+
+set :bundle_cmd, "#{rvm_cmd} && bundle"     
+set :unicorn_command, "#{rvm_cmd} && bundle exec unicorn"
+
+
 task :demo do
-  set :deploy_to, "/home/deploy/apps/nc3_demo"
-  #set :unicorn_command, [ "source ~/.rvm/scripts/rvm", "cd #{deploy_to}/current",
-  #                        "unicorn"].join(" && ")
+  set :application, "nc3_demo"
+  set :deploy_to, "/home/deploy/apps/#{application}"
 end
 
-set :bundle_cmd, [  "source ~/.rvm/scripts/rvm",
-                    "bundle"].join(" && ")
-             
-set :unicorn_command, [ "source ~/.rvm/scripts/rvm", "cd #{deploy_to}/current",
-                        "unicorn"].join(" && ")
 
 
 set :symlinks, {  'config/database.yml' => 'config/database.yml',
-                  'config/config.yml' => 'config/config.yml',
-                  'config/unicorn.rb' => 'config/unicorn.rb',}
+                  'config/config.yml' => 'config/config.yml'}
 
 
 namespace :vlad do
-  remote_task :start_resque do
-    run "source ~/.rvm/scripts/rvm && cd #{deploy_to}/current;
-    RAILS_ENV=production nohup rake environment workers:start > log/workers.log 2>&1 &"
+  remote_task :start_unicorn do
+    run "#{rvm_cmd} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
   end
+  
+  remote_task :stop_unicorn do
+    run "kill `cat #{deploy_to}/current/tmp/pids/unicorn.pid`"
+  end  
+  
+  remote_task :start_resque do
+    run "#{rvm_cmd};
+    RAILS_ENV=production nohup bundle exec rake environment workers:start > log/workers.log 2>&1 &"
+  end
+  
   remote_task :stop_resque do
     run "kill `cat #{deploy_to}/current/tmp/pids/resque/*.pid`"
   end
@@ -38,5 +46,5 @@ end
 
 
 task "vlad:deploy" => %w[
-  vlad:update vlad:symlink vlad:bundle:install vlad:start_app vlad:stop_resque vlad:start_resque vlad:cleanup
+  vlad:update vlad:symlink vlad:bundle:install vlad:stop_unicorn vlad:start_unicorn vlad:stop_resque vlad:start_resque vlad:cleanup
 ]
---
+++ config/unicorn.rb
@@ -0,0 +1,14 @@
+APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
+working_directory APP_ROOT
+worker_processes 2
+preload_app true
+timeout 30
+
+pid  APP_ROOT+ '/tmp/pids/unicorn.pid'
+listen APP_ROOT + "/tmp/sockets/unicorn.sock"
+stderr_path APP_ROOT + "/log/unicorn.stderr.log"
+stdout_path APP_ROOT + "/log/unicorn.stdout.log"
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
