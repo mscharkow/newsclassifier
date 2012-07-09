48609acbf7d91b3f767efe9699148fc042d90f6e
Michael Scharkow <michael@underused.org>
2012-07-09 09:00:44 +0200

    Fix rake tasks in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,27 +6,28 @@ set :repository, "http://github.com/mscharkow/newsclassifier.git"
 set :user, "deploy"
 set :domain, "#{user}@newsclassifier.org"
 set :deploy_to, "/home/deploy/apps/#{application}"
+set :migrate_target, :current
 
-set :rvm_cmd, "source ~/.rvm/scripts/rvm && rvm rvmrc trust #{deploy_to}/current && cd #{deploy_to}/current"
-
+set :rvm_cmd, "source ~/.rvm/scripts/rvm && cd #{deploy_to}/current"
+set :rake_cmd, "#{rvm_cmd} && bundle exec rake"
 set :bundle_cmd, "#{rvm_cmd} && bundle"     
 set :unicorn_command, "#{rvm_cmd} && bundle exec unicorn"
-
 
 task :demo do
   set :application, "nc3_demo"
   set :deploy_to, "/home/deploy/apps/#{application}"
 end
 
-
-
 set :symlinks, {  'config/database.yml' => 'config/database.yml',
                   'config/config.yml' => 'config/config.yml'}
 
-
 namespace :vlad do
+  remote_task :trust_rvm do
+    "#{rvm_cmd} && rvm rvmrc trust #{deploy_to}/current"
+  end
+   
   remote_task :start_unicorn do
-    run "#{rvm_cmd} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+    run "#{rvm_cmd} ; bundle exec unicorn_rails -c config/unicorn.rb -D -E production"
   end
   
   remote_task :stop_unicorn do
@@ -46,5 +47,5 @@ end
 
 
 task "vlad:deploy" => %w[
-  vlad:update vlad:symlink vlad:bundle:install vlad:stop_unicorn vlad:start_unicorn vlad:stop_resque vlad:start_resque vlad:cleanup
+  vlad:update vlad:symlink vlad:trust_rvm vlad:bundle:install vlad:migrate vlad:stop_unicorn vlad:start_unicorn vlad:stop_resque vlad:start_resque vlad:cleanup
 ]
