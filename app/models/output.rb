37b97110bdcfae4ac4255165d49576dfc3c67aa8
Michael Scharkow <michael@underused.org>
2012-08-23 20:12:19 +0200

    Remove stats from CSV (for now).

--- app/models/output.rb
+++ app/models/output.rb
@@ -27,7 +27,7 @@ class Output
   def to_csv(conditions=nil)
     classifiers = @project.classifiers
     classifier_list = classifiers.map(&:variable_name)
-    std = %w(id url title pubdate year month day dayofweek hour minute create_date source words)
+    std = %w(id url title pubdate year month day dayofweek hour minute create_date source)
     header = [std, classifier_list].flatten.join(';')
     out = []
     out << header+"\n"
@@ -35,7 +35,7 @@ class Output
       key = "#{documents.first.classifications.last.try(:cache_key)}#{documents.last.classifications.last.try(:cache_key)}" || Time.now.to_s
       out << Rails.cache.fetch(key) do 
         ha = self.create_hash(classifiers,documents)
-        documents.map {|d| [d.id, clean(d.url), clean(d.title), d.pubdate.strftime('%Y-%m-%d-%H-%M;%Y;%m;%d;%a;%H;%M'), d.created_at.strftime('%Y-%m-%d-%H-%M'), clean(d.source.name), d.stats.fetch(:words,'NA'), classifiers.map{|c| ha[ [c.id,d.id] ] rescue 'NA' }].flatten.join(';')}.join("\n")+"\n"
+        documents.map {|d| [d.id, clean(d.url), clean(d.title), d.pubdate.strftime('%Y-%m-%d-%H-%M;%Y;%m;%d;%a;%H;%M'), d.created_at.strftime('%Y-%m-%d-%H-%M'), clean(d.source.name), classifiers.map{|c| ha[ [c.id,d.id] ] rescue 'NA' }].flatten.join(';')}.join("\n")+"\n"
       end
     end
     out.join
