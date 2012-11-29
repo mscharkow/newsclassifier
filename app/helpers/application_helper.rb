7d9ed0121de4f138a31a22aaa45d018a41e68eab
Michael Scharkow <michael@underused.org>
2012-11-29 10:42:59 +0100

    Version info in layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
   def document_timeline(docs)
     data = docs.count(:group=>'DATE_FORMAT(pubdate,"%y-%m-%d")').map{|k,v|[k,v]}.to_json
   end
+  
+  def version_info
+    `git log -1 | head -n 3`
+  end
 end
