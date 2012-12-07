981a99ba25d50103dc28a73fc9d1a872d9becfb1
Michael Scharkow <michael@underused.org>
2012-12-07 16:10:19 +0100

    Clean up user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,28 +6,22 @@ class User < ActiveRecord::Base
          :encryptable, :encryptor => :restful_authentication_sha1
 
   attr_accessible :email, :login, :password, :password_confirmation
-  
-  
+    
   belongs_to :project
-  
   has_many :documents, :through => :classifications, :uniq => true
   has_many :classifications
   has_and_belongs_to_many :classifiers, :uniq => true
-  
-  def name
-    email
-  end
   
   def fellows
     classifiers.map{|c|c.users.where(['id NOT ?',id])}.flatten.uniq
   end
   
-  validates_presence_of     :email #, :email
-  validates_presence_of     :password
-#  validates_length_of       :password, :within => 4..40, :if => :password_required?
-#  validates_confirmation_of :password,                   :if => :password_required?
-#  validates_length_of       :login,    :within => 3..40
-#  validates_uniqueness_of   :login, :case_sensitive => false
-#  before_save :encrypt_password
+  validates_presence_of :email 
+  validates_presence_of :password
+
+  #TODO - Coding statistics by day of week, hour of day, etc.
+  def stats
+    classifications.size
+  end
     
 end
